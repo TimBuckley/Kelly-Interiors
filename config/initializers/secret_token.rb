600d020c66258a41d74d10d6fbdefaf3df886d5c
Tim Buckley <ninjay@gmail.com>
2014-01-08 14:42:11 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KellyInteriors::Application.config.secret_token = 'aa6d3f9f9870958a0309d1cef2431be36907a65fbf975eae9dab40828d88a8082d331b3d99ccc64572ef422fbd06486906ef580f313a1a1cd3e0c272127fcb96'

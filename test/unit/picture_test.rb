f5471db202cb98ab14b02ed16e9d7dc713bf0165
Tim Buckley <timothypbuckley@gmail.com>
2014-02-18 16:46:50 -0500

    test photo presense

--- test/unit/picture_test.rb
+++ test/unit/picture_test.rb
@@ -4,4 +4,12 @@ class PictureTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  describe Picture do
+    it { should have_attached_file(:photo) }
+    it { should validate_attachment_presence(:photo) }
+    it { should validate_attachment_content_type(:photo).
+      allowing('image/png', 'image/gif').
+      rejecting('text/plain', 'text/xml') }
+  end
 end

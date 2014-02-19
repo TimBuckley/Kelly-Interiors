a7c1d903718d76861cee1fa025b2b288dacb8049
Tim Buckley <timothypbuckley@gmail.com>
2014-02-18 19:17:23 -0500

    add project & picture seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Project.create!(title: "test proj", body: "test desc")
+Picture.create!(project_id: 1, photo: File.new("Pirates4JackSparrowPosterCropped.jpg", "r"))
